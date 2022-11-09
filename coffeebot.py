b1f491ae1b9e8fdd09023f151b1210deb577ba34
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2022-11-09 12:28:23 -0700

    Add files via upload

---
+++ coffeebot.py
@@ -0,0 +1,58 @@
+from utils import print_message, get_size, order_latte
+
+
+def coffee_bot():
+  print('Welcome to the cafe!')
+  order_drink = 'y'
+  drinks = []
+  while order_drink.lower() == 'y':
+    size = get_size()
+    drink_type = get_drink_type()
+    drink = '{} {}'.format(size, drink_type)
+    drinks.append(drink)
+    print('Alright, that\'s a {}!'.format(drink))
+    while True:
+      order_drink = input("Would you like to order another drink? (y/n)\n>")
+      if order_drink in ['y', 'n']:
+        break
+  print("Okay, so I have:\n")
+  for items in drinks:
+    print(items + '\n')
+
+  name = input('Can I get your name please? \n> ')
+  print('Thanks, {}! Your order will be ready shortly.'.format(name))
+
+
+def get_drink_type():
+  res = input(
+      'What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+
+  if res == 'a':
+    return 'brewed coffee'
+  elif res == 'b':
+    return order_mocha()
+  elif res == 'c':
+    return order_latte()
+  else:
+    print_message()
+    return get_drink_type()
+
+# Define new functions here!
+
+
+def order_mocha():
+  while True:
+    res = input("""
+  Would you like to try our limited-edition peppermint mocha?
+[a] Sure!
+[b] Maybe next time!\n>
+    """)
+    if res.lower() == 'a':
+      return 'peppermint mocha'
+    elif res.lower() == 'b':
+      return 'mocha'
+
+    print_message()
+
+
+coffee_bot()
